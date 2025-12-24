353dcd8d776cd467e99d6697dbee5a717aab261d
Penguin <201404847+theWatermelonPenguin@users.noreply.github.com>
2025-12-24 12:27:25 -0700

    feat: added logging to track filling

--- src/controllers/fill.rs
+++ src/controllers/fill.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
 pub fn run(store: &mut HashMap<String, String>, key: String, value: String) {
-    store.insert(key, value);
+    store.insert(key.clone(), value.clone());
+    println!("Filled {} with the value of {}", key, value)
 }
